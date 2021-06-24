c963c63bc15c17d4e52501bc44b2e0067995d1a9
Jérémie MELCHIOR <melchiorjeremie@gmail.com>
2021-06-24 15:29:11 +0200

    premier push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { VoitureComponent } from './voiture/voiture.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoitureComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/voiture/voiture.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voiture',
+  templateUrl: './voiture.component.html',
+  styleUrls: ['./voiture.component.css']
+})
+export class VoitureComponent {
+marque = "renault"
+color = "bleue"
+}
